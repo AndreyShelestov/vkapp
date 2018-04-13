a0c5a5898a708c05dc56700dc2efb3e4e40caabf
Andrey Shelestov <paradygmex@gmail.com>
2018-04-13 16:36:48 +0300

    consts, base request model

--- app/src/main/java/com/example/user/vkapp/consts/ApiConstants.java
+++ app/src/main/java/com/example/user/vkapp/consts/ApiConstants.java
@@ -2,4 +2,6 @@ package com.example.user.vkapp.consts;
 
 public class ApiConstants {
     public static final String[] DEFAULT_LOGIN_SCOPE = {};
+    public static final Double DEFAULT_VERSION = 5.74;
+    public static final int DEFAULT_COUNT = 10;
 }
